e03e3db733046ca4cfa1f0b6d04418e215d9079e
Roger Araujo <araujoroger2402@gmail.com>
2023-02-16 19:56:47 -0400

    se integra el menu mobile

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const mail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const burgerMenu = document.querySelector('.menu');
 
 mail.addEventListener('click', toggleMenu);
 
@@ -7,3 +9,8 @@ function toggleMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
+burgerMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
