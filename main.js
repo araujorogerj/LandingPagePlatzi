ad252a8f7355c7f634096823a0c72f823a960e20
Roger Araujo <araujoroger2402@gmail.com>
2023-02-16 20:23:59 -0400

    se integra el carrito de compra

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
-const mail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-const burgerMenu = document.querySelector('.menu');
+const mail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const burgerMenu = document.querySelector(".menu");
+const cartIcon = document.querySelector(".navbar-shopping-cart");
+const shoppingCart = document.querySelector(".product-detail");
 
-mail.addEventListener('click', toggleMenu);
+mail.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    desktopMenu.classList.toggle('inactive');
+  const isShoppingCartClosed = shoppingCart.classList.contains("inactive");
+
+  if (!isShoppingCartClosed) {
+    shoppingCart.classList.add("inactive");
+  }
+
+  desktopMenu.classList.toggle("inactive");
 }
 
-burgerMenu.addEventListener('click', toggleMobileMenu);
+burgerMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+  const isShoppingCartClosed = shoppingCart.classList.contains("inactive");
+
+  if (!isShoppingCartClosed) {
+    shoppingCart.classList.add("inactive");
+  }
+
+  mobileMenu.classList.toggle("inactive");
+}
+
+cartIcon.addEventListener("click", toggleShoppingCart);
+
+function toggleShoppingCart() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDekstopMenuClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  if (!isDekstopMenuClosed) {
+    desktopMenu.classList.add("inactive");
+  }
+
+  shoppingCart.classList.toggle("inactive");
 }
